80fe9f1bdcae2fdd085675089309c520a227ee1e
shijin <shijindaskp@gmail.com>
2026-06-28 11:19:50 +0530

    cart page locator fix

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -6,11 +6,11 @@ class CartPage(BasePage):
 
     PRODUCT_NAME = (By.CLASS_NAME,"inventory_item_name")
     
-    CHECKOUT_BUTTON = (By.ID,"checkout")
+    CHECKOUT_BUTTON = (By.NAME,"checkout")
 
 
     def checkout_button(self):
-       self.wait_for_page("cart.html")
+
        self.click(self.CHECKOUT_BUTTON)
     
     
